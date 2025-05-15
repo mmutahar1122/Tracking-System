45318469b3f5605d868e5893534e1da00bc8cda3
M. Mutahar <mmutahar38@gamil.com>
2025-05-15 11:04:55 +0500

    set bookseat page

--- src/Pages/Terminals/terminals.jsx
+++ src/Pages/Terminals/terminals.jsx
@@ -2,6 +2,16 @@ import { useEffect } from "react";
 import IMG from "../../assets/bus1.png"
 import AOS from "aos";
 import 'aos/dist/aos.css';
+import faisalabad from '../../assets/Terminals_Img/faisalabad.jpeg'
+import lahore from '../../assets/Terminals_Img/lahore.jpeg'
+import gujranwala from '../../assets/Terminals_Img/gujranwala.jpeg'
+import islamabad from '../../assets/Terminals_Img/Islamabad.jpg'
+import Rawalpindi from '../../assets/Terminals_Img/Rawalpindi.jpg'
+import Multan from '../../assets/Terminals_Img/Multan.jpg'
+import Karachi from '../../assets/Terminals_Img/Karachi.jpg'
+import Peshawar from '../../assets/Terminals_Img/Peshawar.jpg'
+import Quetta from '../../assets/Terminals_Img/Quetta.jpg'
+import Bahawalpure from '../../assets/Terminals_Img/BahawalPure.jpg'
 
 const Terminals=()=>{
  useEffect(() => {
@@ -15,61 +25,61 @@ const busTerminals = [
     city: "Faisalabad",
     terminal: "General Bus Stand",
     address: "Railway Road, General Bus Stand, Faisalabad, Punjab, Pakistan",
-    img: "public/terminals_Img/faisalabad.jpeg"
+    img: faisalabad
   },
   {
     city: "Lahore",
     terminal: "Lahore Daewoo Terminal",
     address: "231 Ferozepur Road, Near Kalma Chowk, Lahore, Punjab, Pakistan",
-    img: "public/terminals_Img/lahore.jpeg"
+    img: lahore
   },
   {
     city: "Gujranwala",
     terminal: "General Bus Stand",
     address: "GT Road, General Bus Stand, Gujranwala, Punjab, Pakistan",
-    img: "public/terminals_Img/gujranwala.jpeg"
+    img: gujranwala
   },
   {
     city: "Islamabad",
     terminal: "Daewoo Express Terminal",
     address: "Motorway Chowk, Main Peshawar Road, Islamabad, Pakistan",
-    img: "public/terminals_Img/Islamabad.jpg"
+    img: islamabad
   },
   {
     city: "Rawalpindi",
     terminal: "Skyways Bus Terminal",
     address: "Opposite Railway Station, Liaquat Road, Rawalpindi, Pakistan",
-    img: "public/terminals_Img/Rawalpindi.jpg"
+    img: Rawalpindi
   },
   {
     city: "Multan",
     terminal: "Faisal Movers Terminal",
     address: "Khanewal Road, Near Chowk Kumharan Wala, Multan, Punjab, Pakistan",
-    img: "public/terminals_Img/Multan.jpg"
+    img: Multan
   },
   {
     city: "Karachi",
     terminal: "Daewoo Express Karachi Terminal",
     address: "Plot #4, Main Super Highway, Sohrab Goth, Karachi, Sindh, Pakistan",
-    img: "public/terminals_Img/Karachi.jpg"
+    img: Karachi
   },
   {
     city: "Peshawar",
     terminal: "General Bus Stand (Haji Camp)",
     address: "GT Road, Near Haji Camp, Peshawar, Khyber Pakhtunkhwa, Pakistan",
-    img: "public/terminals_Img/Peshawar.jpg"
+    img:Peshawar
   },
   {
     city: "Quetta",
     terminal: "Sada Bahar Bus Terminal",
     address: "Sariab Road, Sada Bahar Terminal, Quetta, Balochistan, Pakistan",
-    img: "public/terminals_Img/Quetta.jpg"
+    img: Quetta
   },
   {
     city: "Bahawalpur",
     terminal: "Faisal Movers Bus Terminal",
     address: "Opposite General Bus Stand, Bahawalpur, Punjab, Pakistan",
-    img: "public/terminals_Img/BahawalPure.jpg"
+    img: Bahawalpure
   }
 ];
 
